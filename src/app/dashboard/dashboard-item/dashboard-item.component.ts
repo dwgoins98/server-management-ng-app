dcac375ec5475c03898f4a479ff25a81bef55873
Daniel Goins <dwgoins98@gmail.com>
2025-01-22 16:45:14 -0500

    Used :host to style the components

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -4,11 +4,9 @@ import { Component, input } from '@angular/core';
   selector: 'app-dashboard-item',
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
 })
 export class DashboardItemComponent {
-
-  image = input.required<{src: string, alt: string}>()
-  title = input.required<string>()
-
+  image = input.required<{ src: string; alt: string }>();
+  title = input.required<string>();
 }
