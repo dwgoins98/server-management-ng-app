0b0255f2a9e7fd39b7bfcf9775b01029f919aab5
Daniel Goins <dwgoins98@gmail.com>
2025-01-22 16:23:21 -0500

    Added compnent for control and migrated stylesheets to their compnents - intend to use Angular to fix the stylesheets

--- src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../../ui/button/button.component";
+import { ControlComponent } from "../../../ui/control/control.component";
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ControlComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
