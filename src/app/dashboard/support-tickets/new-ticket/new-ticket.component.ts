ab8505c1223d815ef10e8d32b151c52790d71a33
Daniel Goins <dwgoins98@gmail.com>
2025-01-27 13:59:05 -0500

    feat(new-ticket): update new ticket component
    - Updated the HTML template for the new ticket component to improve layout and styling.
    - Modified the TypeScript logic in the new ticket component to handle form submission and validation.

--- src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,7 +1,9 @@
 import {
   Component,
   ElementRef,
+  model,
   output,
+  signal,
   viewChild,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -16,13 +18,17 @@ import { ControlComponent } from '../../../ui/control/control.component';
 })
 export class NewTicketComponent {
   add = output<{ title: string; ticketText: string }>();
+  enteredTitle = model<string>('');
+  enteredTicketText = model<string>('');
 
   // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
-  onSubmit(title: string, ticketText: string) {
-    this.add.emit({ title, ticketText });
-    this.form().nativeElement.reset();
+  onSubmit() {
+    this.add.emit({ title: this.enteredTitle(), ticketText: this.enteredTicketText() });
+    // this.form().nativeElement.reset();
+    this.enteredTitle.set('');
+    this.enteredTicketText.set('');
   }
   
 }
